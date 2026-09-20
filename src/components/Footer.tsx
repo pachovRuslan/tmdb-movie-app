53abb32e0683a5ecff8a18bdd69472d4a40362b6
Ruslan Pachov <cao-macao@mail.ru>
2026-09-20 22:59:42 +0300

    fix: footer error

--- src/components/Footer.tsx
+++
@@ -1,3 +0,0 @@
-export const Footer = () => {
-    return <footer>© 2026 Kinopoisk Demo · Data courtesy of TMDB.</footer>;
-};
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Footer = () => {
     return <footer>© 2025 Kinopoisk Demo · Data courtesy of TMDB.</footer>;
 };
